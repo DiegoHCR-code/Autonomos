3a23b979f5916b719ed113155c11101f847c1f6b
Diego Henrique Correia Resende <diego_resende01@hotmail.comgit config --global user.name [200~DiegoHCR-code>
2024-04-30 18:41:09 -0300

    Main finalizada

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    $('.banner').slick({
-        infinite: true,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+    $(".owl-carousel").owlCarousel();
 });
